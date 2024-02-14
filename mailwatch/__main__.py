77e356b8cf7b38ca1a29c618b920334c0c1476cc
y <contact@yyyyyyyan.tech>
2024-02-14 19:16:20 -0300

    Go back to package setting. Better organized.

--- mailwatch/__main__.py
+++ mailwatch/__main__.py
@@ -9,37 +9,8 @@ from pathlib import Path
 
 from watchdog.events import FileSystemEventHandler
 
-LOG_LEVELS = {
-    "debug": logging.DEBUG,
-    "info": logging.INFO,
-    "warning": logging.WARNING,
-    "error": logging.ERROR,
-    "critical": logging.CRITICAL,
-}
-
-
-class ColorFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = (
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-    )
-
-    FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset,
-    }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
+from mailwatch.logging import ColorFormatter
+from mailwatch.logging import LOG_LEVELS
 
 
 class NewMailEventHandler(FileSystemEventHandler):
