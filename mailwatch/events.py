22785deabc2859d40e0d12c90bd74b46540454bb
y <contact@yyyyyyyan.tech>
2024-02-19 18:46:06 -0300

    Follow G004.

--- mailwatch/events.py
+++ mailwatch/events.py
@@ -18,17 +18,18 @@ class NewMailEventHandler(LoggingEventHandler):
     def _send_notification(self, **context):
         try:
             cmd = self.notification_handler.get_cmd(**context)
-            self.logger.debug(f"Running command: '{' '.join(cmd)}'")
+            self.logger.debug("Running command: '%s'", " ".join(cmd))
             self.notification_handler.send_notification(*cmd)
         except IconNotFoundError as err:
-            self.logger.error(f"{err} - no icon will be used")
+            self.logger.error("%s - no icon will be used", err)
             self.notification_handler.icon_fmt = None
             self._send_notification(**context)
         except CommandNotFoundError as err:
             self.logger.error(err)
         except CalledProcessError as err:
             self.logger.error(
-                f"Notification command returned an error and exited with status {err.returncode}: {err.stderr.decode('utf8')}"
+                "Notification command returned an error and exited with status %(return_code)s: %(stderr)s",
+                {"return_code": err.returncode, "stderr": err.stderr.decode("utf8")},
             )
 
     def on_created(self, event):
@@ -37,5 +38,5 @@ class NewMailEventHandler(LoggingEventHandler):
             **self.mailbox.get_context(),
             **self.mailbox.get_message_context(Path(event.src_path).name),
         }
-        self.logger.debug(f"Extra context for notification: {context}")
+        self.logger.debug("Extra context for notification: %s", context)
         self._send_notification(**context)
